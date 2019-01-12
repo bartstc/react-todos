0a21fafb9a09ae94b4f0a70ef8e617fdd21527cc
Bart <bartst010395@gmail.com>
2019-01-13 00:50:32 +0100

    Updates

---
+++ precache-manifest.91333acc1961c637dd26c964bd1d926f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d705cbbdf8806c630dfc",
+    "url": "/bartstc/react-todos/static/js/runtime~main.d705cbbd.js"
+  },
+  {
+    "revision": "163335a23f84dab5411d",
+    "url": "/bartstc/react-todos/static/js/main.163335a2.chunk.js"
+  },
+  {
+    "revision": "839a607e68f27c2807e3",
+    "url": "/bartstc/react-todos/static/js/1.839a607e.chunk.js"
+  },
+  {
+    "revision": "163335a23f84dab5411d",
+    "url": "/bartstc/react-todos/static/css/main.604c9888.chunk.css"
+  },
+  {
+    "revision": "b3f5a988e09e05f03bd7d81cc53bde6b",
+    "url": "/bartstc/react-todos/index.html"
+  }
+];
